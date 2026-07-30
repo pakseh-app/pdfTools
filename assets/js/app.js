8ebab61b3661ea2b1864341f7bbdf2b57bd6bd2e
Pakseh <dasbyu6@gmail.com>
2026-07-30 14:58:09 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-console.log("pdfTools Ready");
+const sidebar=document.getElementById("sidebar");
+
+document
+.getElementById("toggleSidebar")
+.onclick=function(){
+
+sidebar.classList.toggle("hide");
+
+};
+
+document
+.querySelectorAll(".menu")
+.forEach(menu=>{
+
+menu.onclick=function(e){
+
+e.preventDefault();
+
+document
+.querySelectorAll(".menu")
+.forEach(x=>x.classList.remove("active"));
+
+this.classList.add("active");
+
+document
+.getElementById("pageTitle")
+.innerText=this.innerText.trim();
+
+};
+
+});
